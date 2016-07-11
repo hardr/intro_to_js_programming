9f63d321905e7368b123bfdbeb380dab01f78e50
Ryan Harings <ryanharings@hotmail.com>
2016-07-11 12:17:15 -0600

    initial commit

---
+++ jsprogramming.js
@@ -0,0 +1,88 @@
+//#1
+for (var i = 0; i <= 1000; i += 100) {
+  console.log(i);
+}
+
+//#2
+for (var i = 1; i <= 128; i *= 2) {
+  console.log(i);
+}
+
+//#3
+for (var i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+//#4
+for (var i = 3; i <= 15; i += 3) {
+  console.log(i);
+}
+
+//#5
+for (var i = 9; i >= 0; i--) {
+  console.log(i);
+}
+
+//#6
+for (var i = 0; i <= 12; i++) {
+  if (i < 3) {
+    console.log(1);
+  }
+  else if (i < 6) {
+    console.log(2);
+  }
+  else if (i < 9) {
+    console.log(3);
+  }
+  else {
+    console.log(4);
+  }
+}
+
+//#7
+var count = 0;
+
+while(count < 3) {
+  for (var i = 0; i <=4; i++) {
+    console.log(i);
+  }
+  count++;
+}
+
+//first bullet
+for (var i = 0; i < 26; i++) {
+  if (i % 2 === 0) {
+    console.log(i + "even")
+  } else {
+    console.log(i + "odd")
+  }
+}
+
+//second bullet
+for (var i = 1; i <= 50; i++) {
+  var whatevs = "";
+  if (i % 3 === 0) {
+    whatevs += "Fizz";
+  }
+  if (i % 5 === 0) {
+    whatevs += "Buzz";
+  }
+  console.log(whatevs || i);
+}
+
+//third bullet
+for (var i = 0; i < 1000; i++) {
+  var multiples = 0;
+  if (i % 3 === 0 || i % 5 === 0) {
+    multiples += i;
+  }
+}
+console.log(multiples);
+
+//fourth bullet
+var stack = "";
+
+for (var i = 0; i < 7; i++) {
+  stack += "#";
+  console.log(stack);
+}
